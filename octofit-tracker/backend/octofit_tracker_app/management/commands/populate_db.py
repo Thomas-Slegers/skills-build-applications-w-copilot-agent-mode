f8fe4df221b5f2ce8e58285216d2c417a6e5aacb
Thomas-Slegers <61152325+Thomas-Slegers@users.noreply.github.com>
2026-05-04 08:27:32 +0000

    Fix populate_db.py: ensure unique index and robust test data creation

--- octofit-tracker/backend/octofit_tracker_app/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker_app/management/commands/populate_db.py
@@ -1,6 +1,8 @@
+
 from django.core.management.base import BaseCommand
 from octofit_tracker_app.models import User, Team, Activity, Workout, Leaderboard
 from django.utils import timezone
+from django.db import connection
 
 class Command(BaseCommand):
     help = 'Populate the octofit_db database with test data'
@@ -12,6 +14,10 @@ class Command(BaseCommand):
         Activity.objects.all().delete()
         Workout.objects.all().delete()
         Leaderboard.objects.all().delete()
+
+        # Ensure unique index on email for users
+        db = connection.cursor().db_conn.client['octofit_db']
+        db.users.create_index('email', unique=True)
 
         # Teams
         marvel = Team.objects.create(name='marvel', description='Marvel superheroes')
@@ -30,11 +36,14 @@ class Command(BaseCommand):
 
         # Activities
         Activity.objects.create(user='ironman@marvel.com', type='run', duration=30, date=timezone.now().date())
+        Activity.objects.create(user='ironman@marvel.com', type='swim', duration=20, date=timezone.now().date())
         Activity.objects.create(user='batman@dc.com', type='cycle', duration=45, date=timezone.now().date())
+        Activity.objects.create(user='superman@dc.com', type='run', duration=60, date=timezone.now().date())
 
         # Workouts
         Workout.objects.create(user='spiderman@marvel.com', name='pushups', reps=20, sets=3, date=timezone.now().date())
         Workout.objects.create(user='superman@dc.com', name='squats', reps=15, sets=4, date=timezone.now().date())
+        Workout.objects.create(user='wonderwoman@dc.com', name='lunges', reps=12, sets=3, date=timezone.now().date())
 
         # Leaderboard
         Leaderboard.objects.create(team='marvel', points=150)
